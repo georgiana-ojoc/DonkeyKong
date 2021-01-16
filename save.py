68a888f747fcedb19515235718d2e93883dd8d25
Ojoc Georgiana Diana <georgiana.ojoc@gmail.com>
2021-01-16 12:11:14 +0200

    Added save GIF functionality in save.py

--- save.py
+++ save.py
@@ -6,20 +6,22 @@ import retro
 
 
 def main():
+    path = os.path.join(os.getcwd(), "GIFs")
+    if not os.path.exists(path):
+        os.mkdir(path)
     environment = retro.make(game="DonkeyKong-Nes")
     episodes = 2
     for episode in range(1, episodes + 1):
-        environment.reset()
         images = []
+        environment.reset()
         score = 0
         done = False
         while not done:
             state, reward, done, _ = environment.step(environment.action_space.sample())
             score += reward
             images += [environment.render(mode="rgb_array")]
-        imageio.mimsave(
-            os.path.join(os.getcwd(), str(score) + ' ' + datetime.now().strftime("%Y-%m-%d %H-%M-%S") + ".gif"),
-            [numpy.array(image) for image in images], fps=30)
+        imageio.mimsave(os.path.join(path, str(score) + ' ' + datetime.now().strftime("%Y-%m-%d %H-%M-%S") + ".gif"),
+                        [numpy.array(image) for image in images], fps=30)
     environment.close()
 
 
