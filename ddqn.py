ae7f02d794119353358f2f9cc78cdc00d1ddc36c
Vladcorjuc <corjuccorneliu@yahoo.it>
2021-01-18 02:21:19 +0200

    update in ddqn.py

--- ddqn.py
+++ ddqn.py
@@ -114,43 +114,6 @@ class ReplayBuffer:
 # numerele 12 si 13 sunt produse de 224/16 si 240/16 rezultate in urma pooling-ului de 4 (efectuat de doua ori)
 
 
-class DuelingDQN(nn.Module):
-    """Convolutional neural network for the Atari games."""
-
-    def __init__(self, num_actions):
-        super(DuelingDQN, self).__init__()
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
-        std = math.sqrt(2.0 / (4 * 84 * 84))
-        nn.init.normal_(self.conv1.weight, mean=0.0, std=std)
-        self.conv1.bias.data.fill_(0.0)
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        std = math.sqrt(2.0 / (32 * 4 * 8 * 8))
-        nn.init.normal_(self.conv2.weight, mean=0.0, std=std)
-        self.conv2.bias.data.fill_(0.0)
-
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        std = math.sqrt(2.0 / (64 * 32 * 4 * 4))
-        nn.init.normal_(self.conv3.weight, mean=0.0, std=std)
-        self.conv3.bias.data.fill_(0.0)
-
-        self.fc1 = nn.Linear(64 * 7 * 7, 512)
-        std = math.sqrt(2.0 / (64 * 64 * 3 * 3))
-        nn.init.normal_(self.fc1.weight, mean=0.0, std=std)
-        self.fc1.bias.data.fill_(0.0)
-        self.V = nn.Linear(512, 1)
-        self.A = nn.Linear(512, num_actions)
-
-    def forward(self, states):
-        """Forward pass of the neural network with some inputs."""
-        x = F.relu(self.conv1(states))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.fc1(x.view(x.size(0), -1)))  # Flatten imathut.
-        V = self.V(x)
-        A = self.A(x)
-        Q = V + (A - A.mean(dim=1, keepdim=True))
-        return Q
 
 
 class Q_Module(nn.Module):
